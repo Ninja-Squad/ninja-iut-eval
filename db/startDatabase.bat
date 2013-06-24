6ccd8c1cac2581d31d54f5d84a9385aa6e1e5e65
clacote <cyril@ninja-squad.com>
2013-06-24 13:36:06 +0200

    Renamed DB to GEEKTIC

--- db/startDatabase.bat
+++ db/startDatabase.bat
@@ -1,1 +1,1 @@
-java -cp hsqldb-2.2.9\hsqldb\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:MyDB --dbname.0 TP_JPA
+java -cp hsqldb-2.2.9\hsqldb\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:Geektic --dbname.0 GEEKTIC
