37c49ec094d36e4120ca3e619405fbfc9c144e68
clacote <cyril@ninja-squad.com>
2013-06-27 14:16:20 +0200

    DB : try to log create.sql errors (not succeeded…)

--- db/create.sh
+++ db/create.sh
@@ -1,1 +1,1 @@
-java -jar hsqldb-2.2.9/hsqldb/lib/sqltool.jar --inlineRc=url=jdbc:hsqldb:hsql://localhost/GEEKTIC,user=sa,password= create.sql > create.log
+java -jar hsqldb-2.2.9/hsqldb/lib/sqltool.jar --inlineRc=url=jdbc:hsqldb:hsql://localhost/GEEKTIC,user=sa,password= $1 2>&1 > create.log 
